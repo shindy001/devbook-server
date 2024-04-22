b54795742626b31674d427127bd0d751ac5c450b
shindy001 <sindelar.p.etr@seznam.cz>
2024-04-22 21:12:01 +0200

    feat: implement DeleteWorkTask mutation

--- src/DevBook.API/Features/TimeTracking/Tasks/DeleteWorkTask.cs
+++ src/DevBook.API/Features/TimeTracking/Tasks/DeleteWorkTask.cs
@@ -1,6 +1,6 @@
 ﻿namespace DevBook.API.Features.TimeTracking.Tasks;
 
-internal record DeleteWorkTaskCommand(Guid Id) : ICommand;
+public sealed record DeleteWorkTaskCommand(Guid Id) : ICommand;
 
 internal class DeleteWorkTaskCommandHandler(DevBookDbContext dbContext) : ICommandHandler<DeleteWorkTaskCommand>
 {
@@ -13,5 +13,15 @@ internal class DeleteWorkTaskCommandHandler(DevBookDbContext dbContext) : IComma
 			dbContext.Tasks.Remove(workTask);
 			await dbContext.SaveChangesAsync(cancellationToken);
 		}
+	}
+}
+
+[MutationType]
+internal sealed class DeleteWorkTaskMutation
+{
+	public async Task<FieldResult<SuccessResult>> DeleteWorkTask(DeleteWorkTaskCommand payload, IExecutor executor, CancellationToken cancellationToken)
+	{
+		await executor.ExecuteCommand(payload, cancellationToken);
+		return new SuccessResult();
 	}
 }
