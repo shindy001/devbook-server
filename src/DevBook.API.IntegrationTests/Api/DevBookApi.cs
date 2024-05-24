c07b9401727200a8ee0afa590dcb56346abf5881
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-24 17:50:13 +0200

    test: specify SudokuApi model with refit interface

---
+++ src/DevBook.API.IntegrationTests/Api/DevBookApi.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+namespace DevBook.API.IntegrationTests.Api;
+
+public interface ISudokuApi
+{
+	[Get("/sudoku")]
+	Task<BoardDataResponse> GetBoardData();
+}
+
+public sealed record BoardDataResponse
+{
+	public ICollection<ICollection<int>> GridNumbers { get; init; } = [];
+	public ICollection<ICollection<int>> SolutionNumbers { get; init; } = [];
+}
