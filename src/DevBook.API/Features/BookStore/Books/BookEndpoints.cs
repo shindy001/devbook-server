eb73d7cd29c6f9332294b23d7490de894b59b740
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 23:13:21 +0200

    feat: implement BookEndpoints, update api definitions

--- src/DevBook.API/Features/BookStore/Books/BookEndpoints.cs
+++ src/DevBook.API/Features/BookStore/Books/BookEndpoints.cs
@@ -39,38 +39,69 @@ internal static class BookEndpoints
 		return groupBuilder;
 	}
 
-	private static async Task<IResult> GetBooks(IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> GetBooks(int? pageSize, int? itemLimit, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteQuery(new GetBooksQuery(PageSize: pageSize, ItemLimit: itemLimit), cancellationToken);
+		return TypedResults.Ok(result);
 	}
 
-	private static async Task<IResult> CreateBook(/*CreateBookCommand command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> CreateBook(CreateBookCommand command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		var result = await executor.ExecuteCommand(command, cancellationToken);
+		return TypedResults.CreatedAtRoute($"{OperationIdPrefix}{GetByIdRoute}", new { id = result.Id });
 	}
 
-	private static async Task<IResult> GetBookById(Guid id, /* IExecutor executor,*/ CancellationToken cancellationToken)
+	private static async Task<IResult> GetBookById(Guid id, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		var result = await executor.ExecuteQuery(new GetBookQuery(id), cancellationToken);
+		return result.Match<IResult>(
+			book => TypedResults.Ok(book),
+			notFound => TypedResults.NotFound(id));
 	}
 
-	private static async Task<IResult> UpdateBook([FromRoute] Guid id, /*UpdateBookCommandDto command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> UpdateBook([FromRoute] Guid id, UpdateBookCommandDto command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteCommand(
+			new UpdateBookCommand(
+				Id: id,
+				Name: command.Name,
+				AuthorId: command.AuthorId,
+				RetailPrice: command.RetailPrice,
+				Price: command.Price,
+				DiscountAmmount: command.DiscountAmmount,
+				Description: command.Description,
+				CoverImageUrl: command.CoverImageUrl,
+				BookCategories: command.BookCategories),
+			cancellationToken);
 
+		return result.Match<IResult>(
+			success => TypedResults.NoContent(),
+			notFound => TypedResults.NotFound(id));
 	}
 
-	private static async Task<IResult> PatchBook([FromRoute] Guid id, /*PatchBookCommandDto command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> PatchBook([FromRoute] Guid id, PatchBookCommandDto command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteCommand(
+			new PatchBookCommand(
+				Id: id,
+				Name: command.Name,
+				AuthorId: command.AuthorId,
+				RetailPrice: command.RetailPrice,
+				Price: command.Price,
+				DiscountAmmount: command.DiscountAmmount,
+				Description: command.Description,
+				CoverImageUrl: command.CoverImageUrl,
+				BookCategories: command.BookCategories),
+		cancellationToken);
 
+		return result.Match<IResult>(
+			success => TypedResults.NoContent(),
+			notFound => TypedResults.NotFound(id));
 	}
 
 	private static async Task<IResult> DeleteBook([FromRoute] Guid id, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		await executor.ExecuteCommand(new DeleteBookCommand(id), cancellationToken);
+		return TypedResults.NoContent();
 	}
 }
