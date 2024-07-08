95509019e96d38e95cf673351ed647e22c059fe7
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 22:13:32 +0200

    feat: implement AuthorEndpoints, update api definitions

--- src/DevBook.API/Features/BookStore/Authors/AuthorEndpoints.cs
+++ src/DevBook.API/Features/BookStore/Authors/AuthorEndpoints.cs
@@ -39,38 +39,57 @@ internal static class AuthorEndpoints
 		return groupBuilder;
 	}
 
-	private static async Task<IResult> GetAuthors(IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> GetAuthors(int? pageSize, int? itemLimit, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteQuery(new GetAuthorsQuery(PageSize: pageSize, ItemLimit: itemLimit), cancellationToken);
+		return TypedResults.Ok(result);
 	}
 
-	private static async Task<IResult> CreateAuthor(/*CreateAuthorCommand command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> CreateAuthor(CreateAuthorCommand command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		var result = await executor.ExecuteCommand(command, cancellationToken);
+		return TypedResults.CreatedAtRoute($"{OperationIdPrefix}{GetByIdRoute}", new { id = result.Id });
 	}
 
-	private static async Task<IResult> GetAuthorById(Guid id,/* IExecutor executor,*/ CancellationToken cancellationToken)
+	private static async Task<IResult> GetAuthorById(Guid id, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		var result = await executor.ExecuteQuery(new GetAuthorQuery(id), cancellationToken);
+		return result.Match<IResult>(
+			author => TypedResults.Ok(author),
+			notFound => TypedResults.NotFound(id));
 	}
 
-	private static async Task<IResult> UpdateAuthor([FromRoute] Guid id, /*UpdateAuthorCommandDto command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> UpdateAuthor([FromRoute] Guid id, UpdateAuthorCommandDto command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteCommand(
+			new UpdateAuthorCommand(
+				Id: id,
+				Name: command.Name,
+				Description: command.Description),
+			cancellationToken);
 
+		return result.Match<IResult>(
+			success => TypedResults.NoContent(),
+			notFound => TypedResults.NotFound(id));
 	}
 
-	private static async Task<IResult> PatchAuthor([FromRoute] Guid id, /*PatchAuthorCommandDto command,*/ IExecutor executor, CancellationToken cancellationToken)
+	private static async Task<IResult> PatchAuthor([FromRoute] Guid id, PatchAuthorCommandDto command, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		var result = await executor.ExecuteCommand(
+			new PatchAuthorCommand(
+				Id: id,
+				Name: command.Name,
+				Description: command.Description),
+		cancellationToken);
 
+		return result.Match<IResult>(
+			success => TypedResults.NoContent(),
+			notFound => TypedResults.NotFound(id));
 	}
 
 	private static async Task<IResult> DeleteAuthor([FromRoute] Guid id, IExecutor executor, CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
-
+		await executor.ExecuteCommand(new DeleteAuthorCommand(id), cancellationToken);
+		return TypedResults.NoContent();
 	}
 }
