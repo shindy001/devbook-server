7ee9173e62047e3dfc4e467076b4e46c84d8838a
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 22:12:21 +0200

    feat: implement GetAuthor action

---
+++ src/DevBook.API/Features/BookStore/Authors/GetAuthor.cs
@@ -0,0 +1,15 @@
+﻿namespace DevBook.API.Features.BookStore.Authors;
+
+internal sealed record GetAuthorQuery(Guid Id) : IQuery<OneOf<Author, NotFound>>;
+
+internal class GetAuthorQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetAuthorQuery, OneOf<Author, NotFound>>
+{
+	public async Task<OneOf<Author, NotFound>> Handle(GetAuthorQuery request, CancellationToken cancellationToken)
+	{
+		var author = await dbContext.Authors.FindAsync([request.Id], cancellationToken);
+
+		return author is null
+			? new NotFound()
+			: author;
+	}
+}
