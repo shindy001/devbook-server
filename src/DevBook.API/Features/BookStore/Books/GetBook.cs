85cdc5c6b0019085319452c129dc0fb373e12670
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 23:12:07 +0200

    feat: implement GetBook action

---
+++ src/DevBook.API/Features/BookStore/Books/GetBook.cs
@@ -0,0 +1,15 @@
+﻿namespace DevBook.API.Features.BookStore.Books;
+
+internal sealed record GetBookQuery(Guid Id) : IQuery<OneOf<Book, NotFound>>;
+
+internal class GetBookQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetBookQuery, OneOf<Book, NotFound>>
+{
+	public async Task<OneOf<Book, NotFound>> Handle(GetBookQuery query, CancellationToken cancellationToken)
+	{
+		var book = await dbContext.Books.FindAsync([query.Id], cancellationToken);
+
+		return book is null
+			? new NotFound()
+			: book;
+	}
+}
