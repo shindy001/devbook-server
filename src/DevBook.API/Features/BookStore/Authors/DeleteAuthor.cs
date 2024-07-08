72ed4145b2ecd3c3c454fbb8dee9591b7626f060
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 22:12:08 +0200

    feat: implement DeleteAuthor action

---
+++ src/DevBook.API/Features/BookStore/Authors/DeleteAuthor.cs
@@ -0,0 +1,17 @@
+﻿namespace DevBook.API.Features.BookStore.Authors;
+
+internal sealed record DeleteAuthorCommand(Guid Id) : ICommand;
+
+internal class DeleteAuthorCommandHandler(DevBookDbContext dbContext) : ICommandHandler<DeleteAuthorCommand>
+{
+	public async Task Handle(DeleteAuthorCommand command, CancellationToken cancellationToken)
+	{
+		var author = await dbContext.Authors.FindAsync([command.Id], cancellationToken);
+
+		if (author is not null)
+		{
+			dbContext.Authors.Remove(author);
+			await dbContext.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
