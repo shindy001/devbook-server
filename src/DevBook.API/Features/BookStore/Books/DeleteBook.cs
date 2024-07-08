c624ab2795f4bb24396eeec3876af643d4e2bc88
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 23:12:01 +0200

    feat: implement DeleteBook action

---
+++ src/DevBook.API/Features/BookStore/Books/DeleteBook.cs
@@ -0,0 +1,17 @@
+﻿namespace DevBook.API.Features.BookStore.Books;
+
+internal sealed record DeleteBookCommand(Guid Id) : ICommand;
+
+internal class DeleteBookCommandHandler(DevBookDbContext dbContext) : ICommandHandler<DeleteBookCommand>
+{
+	public async Task Handle(DeleteBookCommand command, CancellationToken cancellationToken)
+	{
+		var book = await dbContext.Books.FindAsync([command.Id], cancellationToken);
+
+		if (book is not null)
+		{
+			dbContext.Books.Remove(book);
+			await dbContext.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
