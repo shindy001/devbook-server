53e6e94c9a2582246f94b6817e991002f0675118
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 16:07:03 +0200

    feat: add BookStore feature module

---
+++ src/DevBook.API/Features/BookStore/BookStoreModule.cs
@@ -0,0 +1,27 @@
+﻿using DevBook.API.Features.BookStore.Authors;
+using DevBook.API.Features.BookStore.Books;
+
+internal sealed class BookStoreModule : IFeatureModule
+{
+	public IServiceCollection RegisterModule(IServiceCollection services)
+	{
+		return services;
+	}
+
+	public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpointsBuilder)
+	{
+		endpointsBuilder
+			.MapGroup("/bookstore/authors")
+			.MapAuthorEndpoints()
+			.WithTags($"{nameof(BookStoreModule)}_{nameof(AuthorEndpoints)}")
+			.RequireAuthorization(DevBookAccessPolicies.RequireAdmin);
+
+		endpointsBuilder
+			.MapGroup("/bookstore/books")
+			.MapBookEndpoints()
+			.WithTags($"{nameof(BookStoreModule)}_{nameof(BookEndpoints)}")
+			.RequireAuthorization(DevBookAccessPolicies.RequireAdmin);
+
+		return endpointsBuilder;
+	}
+}
