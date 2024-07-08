7d4e0e25053afb26790977f9c76f3eba622d5629
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-08 23:12:17 +0200

    feat: implement GetBooks action

---
+++ src/DevBook.API/Features/BookStore/Books/GetBooks.cs
@@ -0,0 +1,17 @@
+﻿using DevBook.API.Helpers;
+
+namespace DevBook.API.Features.BookStore.Books;
+
+internal sealed record GetBooksQuery(int? PageSize, int? ItemLimit) : IQuery<IEnumerable<Book>>;
+
+internal sealed class GetBooksQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetBooksQuery, IEnumerable<Book>>
+{
+	public async Task<IEnumerable<Book>> Handle(GetBooksQuery query, CancellationToken cancellationToken)
+	{
+		return await dbContext.Books
+			.OrderBy(x => x.Name)
+			.Skip(PagingHelper.NormalizeItemLimit(query.ItemLimit))
+			.Take(PagingHelper.NormalizePageSize(query.PageSize))
+			.ToListAsync(cancellationToken);
+	}
+}
