0e3954061dd0c2d369e21102d7b3ed097a18a063
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-09 23:13:46 +0200

    feat: introduce abstract record Product with default properties

---
+++ src/DevBook.API/Features/BookStore/Shared/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace DevBook.API.Features.BookStore.Shared;
+
+public abstract record Product()
+	: Entity(Guid.NewGuid())
+{
+	public required string Name { get; init; }
+	public required decimal RetailPrice { get; init; }
+	public required decimal Price { get; init; }
+	public required decimal DiscountAmmount { get; init; }
+
+	public string? Description { get; init; }
+	public string? CoverImageUrl { get; init; }
+	public IEnumerable<ProductCategory> ProductCategories { get; init; } = [];
+}
