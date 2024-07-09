09922e43c3c12182f25aadaf1e272544497af096
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-09 22:23:52 +0200

    feat: implement CreateProductCategory action

---
+++ src/DevBook.API/Features/BookStore/ProductCategories/CreateProductCategory.cs
@@ -0,0 +1,26 @@
+﻿namespace DevBook.API.Features.BookStore.ProductCategories;
+
+public sealed record CreateProductCategoryCommand : ICommand<ProductCategory>
+{
+	[Required]
+	public required string Name { get; init; }
+}
+
+public sealed class CreateProductCategoryCommandValidator : AbstractValidator<CreateProductCategoryCommand>
+{
+	public CreateProductCategoryCommandValidator()
+	{
+		RuleFor(x => x.Name).NotEmpty();
+	}
+}
+
+internal sealed class CreateProductCategoryCommandHandler(DevBookDbContext dbContext) : ICommandHandler<CreateProductCategoryCommand, ProductCategory>
+{
+	public async Task<ProductCategory> Handle(CreateProductCategoryCommand command, CancellationToken cancellationToken)
+	{
+		var newItem = new ProductCategory { Name = command.Name };
+		await dbContext.ProductCategories.AddAsync(newItem, cancellationToken);
+		await dbContext.SaveChangesAsync(cancellationToken);
+		return newItem;
+	}
+}
