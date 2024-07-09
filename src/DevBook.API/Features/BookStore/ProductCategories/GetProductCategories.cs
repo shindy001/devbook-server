d3866ec48421d5bcd4e0545c2453ad8dfd2f7987
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-09 22:24:30 +0200

    feat: implement GetProductCategories action

---
+++ src/DevBook.API/Features/BookStore/ProductCategories/GetProductCategories.cs
@@ -0,0 +1,17 @@
+﻿using DevBook.API.Helpers;
+
+namespace DevBook.API.Features.BookStore.ProductCategories;
+
+internal sealed record GetProductCategoriesQuery(int? PageSize, int? ItemLimit) : IQuery<IEnumerable<ProductCategory>>;
+
+internal sealed class GetProductCategoriesQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProductCategoriesQuery, IEnumerable<ProductCategory>>
+{
+	public async Task<IEnumerable<ProductCategory>> Handle(GetProductCategoriesQuery query, CancellationToken cancellationToken)
+	{
+		return await dbContext.ProductCategories
+			.OrderBy(x => x.Name)
+			.Skip(PagingHelper.NormalizeItemLimit(query.ItemLimit))
+			.Take(PagingHelper.NormalizePageSize(query.PageSize))
+			.ToListAsync(cancellationToken);
+	}
+}
