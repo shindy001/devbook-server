a50e4295c61f8ed0623b7e9a29388e5c64d0b7cd
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-09 22:24:06 +0200

    feat: implement DeleteProductCategory action

---
+++ src/DevBook.API/Features/BookStore/ProductCategories/DeleteProductCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace DevBook.API.Features.BookStore.ProductCategories;
+
+internal sealed record DeleteProductCategoryCommand(Guid Id) : ICommand;
+
+internal class DeleteProductCategoryCommandHandler(DevBookDbContext dbContext) : ICommandHandler<DeleteProductCategoryCommand>
+{
+	public async Task Handle(DeleteProductCategoryCommand command, CancellationToken cancellationToken)
+	{
+		var productCategory = await dbContext.ProductCategories.FindAsync([command.Id], cancellationToken);
+
+		// Check for books with this category and throw DevBookError when there are books in this category => cannot delete category with products
+		if (productCategory is not null)
+		{
+			dbContext.ProductCategories.Remove(productCategory);
+			await dbContext.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
