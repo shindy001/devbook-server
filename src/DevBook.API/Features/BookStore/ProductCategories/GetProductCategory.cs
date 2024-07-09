d32f9fa7b8e44955c5b4e109476ff41f2a3bb1a0
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-09 22:24:40 +0200

    feat: implement GetProductCategory action

---
+++ src/DevBook.API/Features/BookStore/ProductCategories/GetProductCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace DevBook.API.Features.BookStore.ProductCategories;
+
+internal sealed record GetProductCategoryQuery(Guid Id) : IQuery<OneOf<ProductCategory, NotFound>>;
+
+internal class GetProductCategoryQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProductCategoryQuery, OneOf<ProductCategory, NotFound>>
+{
+	public async Task<OneOf<ProductCategory, NotFound>> Handle(GetProductCategoryQuery query, CancellationToken cancellationToken)
+	{
+		var productCategory = await dbContext.ProductCategories.FindAsync([query.Id], cancellationToken);
+
+		return productCategory is null
+			? new NotFound()
+			: productCategory;
+	}
+}
