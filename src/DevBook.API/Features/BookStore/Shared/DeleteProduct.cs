37a8f44a69f7bdef451faf45f9658a3fd02fc7c9
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-11 12:03:29 +0200

    feat: implement DeleteProduct action

---
+++ src/DevBook.API/Features/BookStore/Shared/DeleteProduct.cs
@@ -0,0 +1,17 @@
+﻿namespace DevBook.API.Features.BookStore.Shared;
+
+internal sealed record DeleteProductCommand(Guid Id) : ICommand;
+
+internal class DeleteProductCommandHandler(DevBookDbContext dbContext) : ICommandHandler<DeleteProductCommand>
+{
+	public async Task Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+	{
+		var product = await dbContext.Products.FindAsync([command.Id], cancellationToken);
+
+		if (product is not null)
+		{
+			dbContext.Products.Remove(product);
+			await dbContext.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
