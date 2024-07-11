bc59b94393b4553b2935d4b66623e38dc116e807
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-11 12:02:48 +0200

    feat: implement GetProduct action

---
+++ src/DevBook.API/Features/BookStore/Shared/GetProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace DevBook.API.Features.BookStore.Shared;
+
+internal sealed record GetProductQuery(Guid Id) : IQuery<OneOf<Product, NotFound>>;
+
+internal class GetProductQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProductQuery, OneOf<Product, NotFound>>
+{
+	public async Task<OneOf<Product, NotFound>> Handle(GetProductQuery query, CancellationToken cancellationToken)
+	{
+		var product = await dbContext.Products.FindAsync([query.Id], cancellationToken);
+
+		return product is null
+			? new NotFound()
+			: product;
+	}
+}
