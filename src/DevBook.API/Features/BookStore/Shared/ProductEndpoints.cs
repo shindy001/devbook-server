5bfcc632505bad637eaee210d70570d379a76047
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-11 12:03:42 +0200

    feat: implement ProductEndpoints

---
+++ src/DevBook.API/Features/BookStore/Shared/ProductEndpoints.cs
@@ -0,0 +1,46 @@
+﻿namespace DevBook.API.Features.BookStore.Shared;
+
+internal static class ProductEndpoints
+{
+	private const string OperationIdPrefix = "Products.";
+
+	public static RouteGroupBuilder MapProductEndpoints(this RouteGroupBuilder groupBuilder)
+	{
+		groupBuilder.MapGet("/", GetProducts)
+			.WithName($"{OperationIdPrefix}GetAll")
+			.Produces<IList<Product>>()
+			.AllowAnonymous();
+
+		groupBuilder.MapGet("/{id:guid}", GetProductById)
+			.WithName($"{OperationIdPrefix}{ApiConstants.GetByIdRoute}")
+			.Produces<Product>()
+			.Produces(StatusCodes.Status404NotFound)
+			.AllowAnonymous();
+
+		groupBuilder.MapDelete("/{id:guid}", DeleteProduct)
+			.WithName($"{OperationIdPrefix}Delete")
+			.Produces(StatusCodes.Status204NoContent);
+
+		return groupBuilder;
+	}
+
+	private static async Task<IResult> GetProducts(int? pageSize, int? itemLimit, IExecutor executor, CancellationToken cancellationToken)
+	{
+		var result = await executor.ExecuteQuery(new GetProductsQuery(PageSize: pageSize, ItemLimit: itemLimit), cancellationToken);
+		return TypedResults.Ok(result);
+	}
+
+	private static async Task<IResult> GetProductById(Guid id, IExecutor executor, CancellationToken cancellationToken)
+	{
+		var result = await executor.ExecuteQuery(new GetProductQuery(id), cancellationToken);
+		return result.Match<IResult>(
+			book => TypedResults.Ok(book),
+			notFound => TypedResults.NotFound(id));
+	}
+
+	private static async Task<IResult> DeleteProduct([FromRoute] Guid id, IExecutor executor, CancellationToken cancellationToken)
+	{
+		await executor.ExecuteCommand(new DeleteProductCommand(id), cancellationToken);
+		return TypedResults.NoContent();
+	}
+}
