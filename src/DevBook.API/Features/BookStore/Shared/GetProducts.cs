3eb07e6279caf663f5e25267656e8ece81b5dea3
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-11 12:02:55 +0200

    feat: implement GetProducts action

---
+++ src/DevBook.API/Features/BookStore/Shared/GetProducts.cs
@@ -0,0 +1,17 @@
+﻿using DevBook.API.Helpers;
+
+namespace DevBook.API.Features.BookStore.Shared;
+
+internal sealed record GetProductsQuery(int? PageSize, int? ItemLimit) : IQuery<IEnumerable<Product>>;
+
+internal sealed class GetProductsQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProductsQuery, IEnumerable<Product>>
+{
+	public async Task<IEnumerable<Product>> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+	{
+		return await dbContext.Products
+			.OrderBy(x => x.Name)
+			.Skip(PagingHelper.NormalizeItemLimit(query.ItemLimit))
+			.Take(PagingHelper.NormalizePageSize(query.PageSize))
+			.ToListAsync(cancellationToken);
+	}
+}
