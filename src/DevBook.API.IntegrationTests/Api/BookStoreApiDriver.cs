e182d77b69d98a8f2bfbd5941f30fcd9bd01bab7
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-13 00:32:34 +0200

    test: extract seed functions to BookStoreApiDriver, wrap calls with auth handler to ensure authentication when seeding

---
+++ src/DevBook.API.IntegrationTests/Api/BookStoreApiDriver.cs
@@ -0,0 +1,64 @@
+﻿using DevBook.API.Features.BookStore.Authors;
+using DevBook.API.Features.BookStore.Products.Books;
+using Microsoft.AspNetCore.Authentication;
+using System.Net;
+
+namespace DevBook.API.IntegrationTests.Api;
+
+/// <summary>
+/// Driver that executes actions on BookStoreApi.
+/// Requires created IBookStoreApi client.
+/// </summary>
+internal sealed class BookStoreApiDriver
+{
+	private readonly IBookStoreApi _bookStoreApi;
+	private readonly TestAuthInterceptor _testAuthInterceptor;
+
+	public BookStoreApiDriver(IBookStoreApi bookStoreApi, TestAuthInterceptor testAuthInterceptor)
+	{
+		_bookStoreApi = bookStoreApi;
+		_testAuthInterceptor = testAuthInterceptor;
+	}
+
+	public async Task<Guid> SeedAuthor(CreateAuthorCommand createCommand)
+	{
+		return await ExecuteInAdminAuthContext(async () =>
+		{
+			var response = await _bookStoreApi.CreateAuthor(createCommand);
+			return ExtractCreatedLocationGuid(response);
+		});
+	}
+
+	public async Task<Guid> SeedBook(CreateBookCommand createCommand)
+	{
+		return await ExecuteInAdminAuthContext(async () =>
+		{
+			var response = await _bookStoreApi.CreateBook(createCommand);
+			return ExtractCreatedLocationGuid(response);
+		});
+	}
+
+	private async Task<Guid> ExecuteInAdminAuthContext(Func<Task<Guid>> action)
+	{
+		var currentAuthentication = _testAuthInterceptor.OnAuthenticate?.Clone() as Func<Task<AuthenticateResult>>;
+		var result = await action();
+		_testAuthInterceptor.OnAuthenticate = currentAuthentication;
+
+		return result;
+	}
+
+	private async Task ExecuteInAdminAuthContext(Func<Task<Action>> action)
+	{
+		var currentAuthentication = _testAuthInterceptor.OnAuthenticate?.Clone() as Func<Task<AuthenticateResult>>;
+		_testAuthInterceptor.AuthenticateAdmin();
+		await action();
+		_testAuthInterceptor.OnAuthenticate = currentAuthentication;
+	}
+
+	private Guid ExtractCreatedLocationGuid(HttpResponseMessage response)
+	{
+		response.StatusCode.Should().Be(HttpStatusCode.Created);
+		var id = response.Headers.Location?.Segments[^1];
+		return Guid.Parse(id!);
+	}
+}
--- src/DevBook.API.IntegrationTests/Api/IBookStoreApi.cs
+++ src/DevBook.API.IntegrationTests/Api/IBookStoreApi.cs
@@ -1,7 +1,5 @@
 ﻿using DevBook.API.Features.BookStore.Authors;
-using DevBook.API.Features.BookStore.Products;
 using DevBook.API.Features.BookStore.Products.Books;
-using System.Net;
 
 namespace DevBook.API.IntegrationTests.Api;
 
@@ -13,10 +11,10 @@ public interface IBookStoreApi
 	Task<IList<T>> GetProducts<T>();
 
 	[Get("/bookstore/products/{id}")]
-	Task<Product> GetProductById(Guid id);
+	Task<T> GetProductById<T>(Guid id);
 
 	[Delete("/bookstore/products/{id}")]
-	Task DeleteProduct(Guid id);
+	Task<HttpResponseMessage> DeleteProduct(Guid id);
 
 	#endregion
 
@@ -33,26 +31,4 @@ public interface IBookStoreApi
 	Task<HttpResponseMessage> CreateBook(CreateBookCommand command);
 
 	#endregion
-}
-
-internal static class BookStoreApiExtensions
-{
-	public static async Task<Guid> SeedAuthor(this IBookStoreApi bookStoreApi, CreateAuthorCommand createCommand)
-	{
-		var response = await bookStoreApi.CreateAuthor(createCommand);
-		return ExtractCreatedLocationGuid(response);
-	}
-
-	public static async Task<Guid> SeedBook(this IBookStoreApi bookStoreApi, CreateBookCommand createCommand)
-	{
-		var response = await bookStoreApi.CreateBook(createCommand);
-		return ExtractCreatedLocationGuid(response);
-	}
-
-	private static Guid ExtractCreatedLocationGuid(HttpResponseMessage response)
-	{
-		response.StatusCode.Should().Be(HttpStatusCode.Created);
-		var id = response.Headers.Location?.Segments[^1];
-		return Guid.Parse(id!);
-	}
 }
