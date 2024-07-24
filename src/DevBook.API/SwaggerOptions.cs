d1ab2b19dee3a0ecc488921b79c5810b529817f4
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-24 23:17:32 +0200

    refactor: remove SwaggerIdentityOperationIdFilter as it is not needed with custom identity endpoints

--- src/DevBook.API/SwaggerOptions.cs
+++ src/DevBook.API/SwaggerOptions.cs
@@ -34,46 +34,7 @@ public static class SwaggerOptions
 				}
 			});
 
-			opt.OperationFilter<SwaggerIdentityOperationIdFilter>();
 			opt.UseAllOfToExtendReferenceSchemas();
 		};
-	}
-
-	/// <summary>
-	/// Assignes missing OperationIds to .net8 Identity actions added with MapIdentityApi, endpoints should be grouped as 'identity'
-	/// Also acts as workaround for swaggergen/nswag connected service conflict of .net 8 2fa action which is by default generated as 2fa function which is not valid c# name
-	/// <see href="https://github.com/dotnet/aspnetcore/issues/49720" />
-	/// </summary>
-	private class SwaggerIdentityOperationIdFilter : IOperationFilter
-	{
-		private const string OperationIdPrefix = "Identity.";
-
-		public void Apply(OpenApiOperation operation, OperationFilterContext context)
-		{
-			// Check if it's a Identity action
-			if (context.ApiDescription.RelativePath?.StartsWith("identity", StringComparison.OrdinalIgnoreCase) == true)
-			{
-				var relativePath = context.ApiDescription.RelativePath ?? string.Empty;
-				// Add operationId to Identity actions, also add HttpMethod at the end to avoid duplicates
-				operation.OperationId = $"{OperationIdPrefix}{GenerateOperationIdBaseString(relativePath)}{context.ApiDescription.HttpMethod}";
-				return;
-			}
-		}
-
-		private static string GenerateOperationIdBaseString(string inputString)
-		{
-			var input = inputString.Split('/');
-			if (input.Length == 0)
-			{
-				return string.Empty;
-			}
-
-			return string.Join(string.Empty, input.Skip(1).Select(CapitalizeFirstLetter));
-		}
-
-		private static string CapitalizeFirstLetter(string input)
-		{
-			return string.Concat(input.First().ToString().ToUpper(), input.AsSpan(1));
-		}
 	}
 }
